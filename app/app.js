71f1196fb84d6900d122a9cfbe5f59a4be8150a7
Alex Sweeney <alexsweeney@live.co.uk>
2017-05-14 17:16:57 +0700

    split files

--- app/app.js
+++ app/app.js
@@ -6,169 +6,18 @@
 	x The calendar should display as many rows for weeks as required for the month. The left most cell is for Sundays, and the right most for Saturdays.
 	
 	x Any padded days from the previous/next month should be visually delineated from the days in the current month (as in the sketch above).
+
+	made directive modular 
 	
 	Once your directive is working, write unit tests for your controller(s) and directive.
 	
 	Use Git/Github for version control.
 
+	compare with thinkful version
+
 */
 (function() {
 	'use strict';
 
-	angular.module('calendarDemoApp', [])
-		.factory('Months', Months)
-		.factory('CalendarRange', CalendarRange)
-		.run(function($rootScope, Months, CalendarRange) {
-			var init = function() {
-				setYears();
-				setMonth();
-				setDays();
-			}
-
-			var setYears = function() {
-				$rootScope.years = [];
-				var date = new Date();
-				var thisYear = date.getFullYear();
-
-				var startYear = thisYear - 20;
-				var finalYear = thisYear + 20;
-
-				for(var i=0; i<40; i++) {
-					$rootScope.years.push(startYear + i);
-				}
-
-				$rootScope.selectedYear = thisYear;
-			}
-
-			var setMonth = function() {
-				var date = new Date();
-				var thisMonth = Months[date.getMonth()];   
-
-				$rootScope.months = Months;
-				$rootScope.selectedMonth = thisMonth;
-			}
-
-			var returnMonthNumber = function(targetMonth) {
-				var count = 1;
-
-				for(var i in Months) {
-					var month = Months[i];
-
-					if(month == targetMonth) {
-						return count;
-					} else {
-						count ++;
-					}  
-				}
-			}
-
-			var retrunSelectedDate = function() {
-				var year = String($rootScope.selectedYear).substring(2, 4);
-				var month = returnMonthNumber($rootScope.selectedMonth); 
-
-				var date = new Date(year, month, 1);
-				return date;
-			}
-
-			var setDays = function() {
-				var range = CalendarRange.getMonthlyRange(retrunSelectedDate() );
-
-				$rootScope.days = range.days; 
-
-				$rootScope.start = range.start;
-				$rootScope.end = range.end; 
-
-				console.log($rootScope.end);
-			} 
-
-			$rootScope.returnIfOutOfRange = function(date) { 
-				if(date < $rootScope.start || date > $rootScope.end ) { 
-					return true;
-				} else { 
-					return false;
-				}
-			}
-
-			init();
-
-			$rootScope.selectMonth = function(month) {
-				$rootScope.selectedMonth = month;
-
-				setDays();
-			}
-
-			$rootScope.selectYear = function(year) {
-				$rootScope.selectedYear = year;
-
-				setDays();
-			}
-			
-		})
-		.directive('calendar', function(Months) {
-			return {
-				restrict: 'E',
-				templateUrl: 'calendar-template.html'
-			}
-		});
-
-	function Months() {
-		return ['Janurary', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	}
-
-	function CalendarRange() {
-		return {
-			DAY : 24 * 60 * 60 * 1000,
-
-			prepareDate : function(date) {
-			    date = new Date(date);
-			    var day = date.getDay();
-			    return {
-			      date : date,
-			      weekday : day != 0 && day != 6,
-			      day : date.getDate(),
-			      month : date.getMonth(),
-			      year : date.getFullYear()
-			    };
-			},
-
-			getMonthlyRange : function(date) {
-			    var month = date.getMonth();
-			    var startDay = new Date(date);
-			    startDay.setDate(1);
-
-			    var firstDay = new Date(startDay);
-			    if (firstDay.getDay() > 0) { //Not Sunday
-			      firstDay.setDate(firstDay.getDate() - (firstDay.getDay()));
-			    }
-
-			    var endDay = new Date(startDay);
-			    if(month == 11) {
-			      endDay.setMonth(0);
-			      endDay.setYear(endDay.getFullYear() + 1);
-			    } else {
-			      endDay.setMonth(month + 1);
-			    }
-
-			    endDay.setDate(endDay.getDate() - 1);
-
-			    var lastDay = new Date(endDay);
-			    lastDay.setDate(lastDay.getDate() + (6 - endDay.getDay()));
-
-			    var day = new Date(firstDay);
-			    var days = [];
-			    while(day <= lastDay) {
-			      days.push(this.prepareDate(day));
-			      day.setDate(day.getDate() + 1);
-			    }
-
-			    return {
-			      first : firstDay,
-			      start : startDay,
-			      end : endDay,
-			      last : lastDay,
-			      days : days
-			    };
-			} 
-		}
-	} 
+	angular.module('calendarDemoApp', []) 
 }());
---
+++ app/calendar-directive/calendar.directive.js
@@ -0,0 +1,98 @@
+(function() {
+	'use strict';
+
+	angular.module('calendarDemoApp')
+
+	.directive('calendar', function(CalendarRange) {
+		return {
+			restrict: 'E',
+			templateUrl: 'calendar-directive/calendar.template.html',
+			link: function(scope, element, attrs) { 
+				var months = ['Janurary', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+				
+				// utility
+					var returnMonthNumber = function(targetMonth) {
+						var count = 1;
+
+						for(var i in months) {
+							var month = months[i];
+
+							if(month == targetMonth) {
+								return count;
+							} else {
+								count ++;
+							}  
+						}
+					}
+
+					var retrunSelectedDate = function() {
+						var year = String(scope.selectedYear).substring(2, 4);
+						var month = returnMonthNumber(scope.selectedMonth); 
+
+						var date = new Date(year, month, 1);
+						return date;
+					}
+
+				// interact
+					scope.returnIfOutOfRange = function(date) { 
+						if(date < scope.start || date > scope.end ) { 
+							return true;
+						} else { 
+							return false;
+						}
+					} 
+
+					scope.selectMonth = function(month) {
+						scope.selectedMonth = month;
+
+						setDays();
+					}
+
+					scope.selectYear = function(year) {
+						scope.selectedYear = year;
+
+						setDays();
+					}
+
+				// init 
+					var setYears = function() {
+						scope.years = [];
+						var date = new Date();
+						var thisYear = date.getFullYear();
+
+						var startYear = thisYear - 20;
+						var finalYear = thisYear + 20;
+
+						for(var i=0; i<40; i++) {
+							scope.years.push(startYear + i);
+						}
+
+						scope.selectedYear = thisYear;
+					}
+
+					var setMonth = function() {
+						var date = new Date();
+						var thisMonth = months[date.getMonth()];   
+
+						scope.months = months;
+						scope.selectedMonth = thisMonth;
+					}
+
+					var setDays = function() {
+						var range = CalendarRange.getMonthlyRange(retrunSelectedDate() );
+
+						scope.days = range.days; 
+
+						scope.start = range.start;
+						scope.end = range.end;  
+					} 
+
+					var init = function() {
+						setYears();
+						setMonth();
+						setDays();
+					}();
+			}
+		}
+	});
+}())
