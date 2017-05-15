1e644d718c9f5d666bcc5e51274f289a69607159
Alex Sweeney <alexsweeney@live.co.uk>
2017-05-15 12:57:48 +0700

    calendarRange test

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,11 @@
 // Karma configuration
-// Generated on Wed Jul 16 2014 15:23:43 GMT-0400 (EDT)
+// Generated on Mon May 15 2017 11:30:53 GMT+0700 (SE Asia Standard Time)
 
 module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: 'app',
 
 
     // frameworks to use
@@ -15,23 +15,34 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'calendarRange.js',
-      'calendarRangeSpec.js'
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/bootstrap/dist/js/bootstrap.min.js', 
+        'bower_components/angular/angular.min.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+
+        'app.js',
+        'calendar-directive/*.js',
+        'calendar-directive/*.html',
+
+        'tests/*.js',
     ],
 
 
     // list of files to exclude
     exclude: [
-      
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+        'src/directive/*.html': 'ng-html2js'
     },
 
+    ngHtml2JsPreprocessor: {
+        // strip app from the file path
+        stripPrefix: 'src/directive/'
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
@@ -64,5 +75,5 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  });
-};
+  })
+}
